40c1435f34e1aea45a220163f2e46158f72ba776
okutagawa <diziek182@gmail.com>
2025-03-29 17:06:49 +0400

    Done first minigame

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using PrimeTween;
+
+
+public class Card : MonoBehaviour
+{
+    [SerializeField] private Image iconImage;
+
+    public Sprite hiddenIconSprite;
+    public Sprite iconSprite;
+
+    public bool isSelected;
+
+    public CardsController controller;
+
+    public void OnCardClick()
+    {
+        controller.SetSelected(this);
+    }
+
+    public void SetIconSprite(Sprite sp)
+    {
+        iconSprite = sp;
+    }
+
+    public void Show()
+    {
+        Tween.Rotation(transform, // таргет
+            new Vector3(0f, 180f, 0f), // 180  градусов
+            0.2f); // 2 секунды
+
+        Tween.Delay(0.1f, () => iconImage.sprite = iconSprite);
+        isSelected = true;
+    }
+
+    public void Hide()
+    {
+        Tween.Rotation(transform,
+            new Vector3(0f, 0f, 0f),
+            0.2f);
+
+        Tween.Delay(0.1f, () =>
+        {
+            iconImage.sprite = hiddenIconSprite;
+            isSelected = false;
+        });
+    }
+}
