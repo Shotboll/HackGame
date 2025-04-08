585b705360a05abbe0f68b57bd6615ab758b7555
okutagawa <diziek182@gmail.com>
2025-04-09 02:17:49 +0400

    Need Modificaiton =)

---
+++ Assets/Scripts/MainMenu/MainMenuObj.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuObj : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Игра закрылась");
+        Application.Quit();
+    }
+}
