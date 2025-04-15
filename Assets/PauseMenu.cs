3ed69d312758dcff87f365bcaa721fa576014785
Shotboll <95362093+Shotboll@users.noreply.github.com>
2025-04-15 22:32:11 +0400

    Worked menu

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool PauseGame;
+    public GameObject pauseGameMenu;
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (PauseGame)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseGameMenu.SetActive(false);
+        Time.timeScale = 1.0f;
+        PauseGame = false;
+    }
+
+    public void Pause()
+    {
+        pauseGameMenu.SetActive(true);
+        Time.timeScale = 0f;
+        PauseGame = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
